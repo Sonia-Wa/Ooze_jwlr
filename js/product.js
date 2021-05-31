37bf8e3504dfa49000d894eed13552b1ecdd0ff1
Sonia-Wa <78491644+Sonia-Wa@users.noreply.github.com>
2021-05-31 10:27:51 +0200

    Add files via upload

---
+++ js/product.js
@@ -0,0 +1,46 @@
+const urlParams = new URLSearchParams(window.location.search);
+
+//in the URL grab ID and store it's value in id
+const id = urlParams.get("id");
+
+const url = "https://kea21s2-040e.restdb.io/rest/ooze-database/" + id;
+
+const key = {
+    headers: {
+      "x-apikey": "606d5f59f55350043100750e	",
+    },
+  };
+
+
+
+  fetch(url, key)
+  .then((res) => res.json())
+  .then((data) => showProduct(data));
+
+
+    function showProduct(jewelry) {
+        //template
+        const template = document.querySelector("#prodview").content;
+
+        //clone
+        const copy = template.cloneNode(true);
+
+        //content change
+        copy.querySelector("img").src = jewelry.image;
+
+        copy.querySelector(".piece-name").textContent = jewelry.name;
+
+        copy.querySelector(".piece-price").textContent = `${jewelry.price} PLN`;
+
+        copy.querySelector(".piece-desc").textContent = jewelry.description;
+
+        copy.querySelector(".piece-mat").textContent = jewelry.material;
+
+        copy.querySelector(".piece-dim").textContent = jewelry.dimentions;
+
+        //grab parent
+        const parent = document.querySelector(".product_inf");
+        //append child
+        parent.appendChild(copy);
+
+    }
--- js/productlist.js
+++ js/productlist.js
@@ -1,3 +1,48 @@
+const urlParams = new URLSearchParams(window.location.search);
+
 const url = "https://kea21s2-040e.restdb.io/rest/ooze-database";
 
-fetch (url) 
+const key = {
+    headers: {
+      "x-apikey": "606d5f59f55350043100750e	",
+    },
+  };
+
+
+
+fetch (url, key) 
+    .then(function (res) {
+        console.log(res);
+        return res.json();
+    })
+    .then(function (data){
+        allProducts(data);
+    });
+
+    function allProducts(data) {
+        data.forEach(showProducts);
+    }
+
+    function showProducts(jewelry) {
+        //template
+        const template = document.querySelector("#prod-temp").content;
+
+        //clone
+        const copy = template.cloneNode(true);
+
+        //content change
+        copy.querySelector(".prodview-link").href =  `product.html?id=${jewelry._id}`;
+
+        copy.querySelector("img").src = jewelry.image;
+
+        copy.querySelector(".piece-name").textContent = jewelry.name;
+
+        copy.querySelector(".piece-price").textContent = `${jewelry.price} PLN`;
+
+        //grab parent
+        const parent = document.querySelector("main");
+        //append child
+        parent.appendChild(copy);
+
+    }
+
